585408ee03aed1d469c016f7ec0c4b165ec42d93
sevgilisedat <sedatsevgili@gmail.com>
2017-10-27 09:36:33 +0300

    installed vuex and elementui

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
